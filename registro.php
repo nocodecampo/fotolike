e812be9a59ba927279e499aa3ff9e694856396a7
nocodecampo <dkampo@gmail.com>
2025-01-16 17:06:20 +0100

    registro

--- registro.php
+++ registro.php
@@ -2,25 +2,32 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
     <main>
-        <h1>Regístrate y a callar:</h1>
         <section>
+        <h1>Regístrate y a callar:</h1>
             <form action="" method="post">
-                <label for="nombre">Nombre</label>
-                <input type="text" name="nombre" id="nombre" placeholder="Nombre" required>
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email" placeholder="Email" required>
-                <label for="password">Contraseña</label>
-                <input type="password" name="password" id="password" placeholder="Contraseña" required>
+                <div>
+                    <label for="nombre">Nombre<span>*</span></label>
+                    <input type="text" name="nombre" id="nombre" placeholder="Nombre" required>
+                </div>
+                <div>
+                    <label for="email">Email<span>*</span></label>
+                    <input type="email" name="email" id="email" placeholder="Email" required>
+                </div>
+                <div>
+                    <label for="password">Contraseña<span>*</span></label>
+                    <input type="password" name="password" id="password" placeholder="Contraseña" required>
+                </div>
                 <button type="submit">Registrarse</button>
             </form>
         </section>
